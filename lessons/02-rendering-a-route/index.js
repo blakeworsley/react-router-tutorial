63d242304c3a43b0814ce656903edf011c136987
Blake Worsley <blakeworsley@gmail.com>
2016-10-03 21:35:37 -0600

    working through active

--- lessons/02-rendering-a-route/index.js
+++ lessons/02-rendering-a-route/index.js
@@ -1,4 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Router, Route, hashHistory } from 'react-router'
 import App from './modules/App'
-render(<App/>, document.getElementById('app'))
+
+render((
+  <Router history={hashHistory}>
+    <Route path="/" component={App}/>
+    <Route path="/repos" component={Repos} />
+    <Route path="/about" component={About} />
+  </Router>
+), document.getElementById('app'))
